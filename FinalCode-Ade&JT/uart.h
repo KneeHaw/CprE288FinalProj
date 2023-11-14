72e47ab9dde7c861e53b20f510d8c8b1b9e62cbb
lvrd-ade <fadahunsiadeife11@gmail.com>
2023-11-14 16:47:53 -0600

    new code

---
+++ FinalCode-Ade&JT/uart.h
@@ -0,0 +1,32 @@
+/*
+*
+*   uart.h
+*
+*   Used to set up the UART
+*   uses UART1 at 115200
+*
+*
+*   @author Dane Larson
+*   @date 07/18/2016
+*   Phillip Jones updated 9/2019, removed WiFi.h
+*/
+
+#ifndef UART_H_
+#define UART_H_
+
+#include "Timer.h"
+#include <inc/tm4c123gh6pm.h>
+
+void uart_init(void);
+
+void uart_sendChar(char data);
+
+char uart_receive(void);
+
+void uart_sendStr(const char *data);
+
+void uart_interrupt_init();
+void uart1_handler();
+
+
+#endif
