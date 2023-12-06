bc11e24e1572faa510326eaac7f288c0a634b982
KneeHaw <90938928+KneeHaw@users.noreply.github.com>
2023-12-06 10:27:05 -0600

    Add files via upload

---
+++ Cpre288_FinalCode_Zip/Socket/database.py
@@ -0,0 +1,220 @@
+import datetime
+import os
+from xata.client import XataClient
+import xata
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
+def getClient() -> XataClient:
+    DB_URL = os.getenv("DB_URL")
+    API_KEY = os.getenv("API_KEY")
+    if DB_URL is None:
+        raise ValueError("DB_URL is not defined")
+    if API_KEY is None:
+        raise ValueError("API_KEY is not defined")
+    return XataClient(db_url=DB_URL, api_key=API_KEY)
+
+
+# marker has a date, lat, long
+
+
+class Marker:
+    def __init__(self, date, lat, long):
+        self.date = date
+        self.lat = lat
+        self.long = long
+
+    def __str__(self):
+        return f"{self.date} {self.lat} {self.long}"
+
+
+# Adds a path marker to the database which shows the path the robot has taken
+def addPathMarker(date, lat, long) -> bool:
+    client = getClient()
+    record = {"date": date, "lat": lat, "long": long}
+    client.records().insert("paths", record, "main")
+    return True
+
+
+# Gets all the paths from the database
+def getPaths():
+    client = getClient()
+    records = client.records()
+    results = records.get("paths", "all")
+    return results
+
+
+def getDestination():
+    client = getClient()
+    records = client.records()
+    results = records.get("order", "all")
+    return results
+
+
+# Gets the current date and time in the database format
+
+
+def getOrder():
+    """Returns a list of orders with the following columns: id, goods,
+    total_price, house, client. The house and client columns are
+    foreign keys to the houses and clients table. The orders are the
+    orders that are in the database to be delivered."""
+    # Get the xata client
+    client = getClient()
+
+    data = client.data().query(
+        "orders",
+        {
+            "columns": [
+                "id",
+                "goods",
+                "total_price",
+                "house",
+                "client",
+                "house.character",
+            ]
+        },
+    )
+    # Reduce to one order
+    print(data)
+    return data
+
+
+def getLatestOrder():
+    """Returns the latest order in the database"""
+    # Get the xata client
+    client = getClient()
+
+    # Quert Database for orders
+    data = client.data().query(
+        "orders",
+        {
+            "columns": [
+                "house.character",
+                "status",
+                "id"
+            ],
+        },
+    )
+
+    # Reduce to one order
+    data = data.get("records")
+    # now data is a list
+    if (len(data)==0): return None
+    data = data[0]
+    print(data)
+    data = [data.pop("house").pop("character"), data.pop("status"), data.pop("id")]
+    return data
+
+
+def createObstacle(latitude: float, longitude: float):
+    """Creates an obstacle with the given latitude and longitude
+    in the database. Returns the data of the obstacle. Is
+    Shown on the map as a obstacle"""
+
+    # Get the xata client
+    client = getClient()
+
+    # Insert the obstacle
+    data = client.records().insert(
+        "obstacles", {"latitude": latitude, "longitude": longitude}
+    )
+
+    # Return the data
+    return data
+
+
+def getHouses():
+    """Returns a list of houses with the following columns:
+    id, latitude, longitude, photo, name, character"""
+
+    # Get the xata client
+    client = getClient()
+
+    # Quert Database for houses
+    data = client.data().query(
+        "houses",
+        {"columns": ["id", "latitude", "longitude", "photo", "name", "character"]},
+    )
+    return data
+
+
+def updateOrderStatus(order_id: str, status: str):
+    """A Status can be: "pending", "in_progress", "completed" or "cancelled"""
+    # Get the xata client
+    client = getClient()
+
+    # Update the order status
+    data = client.records().update("order", order_id, {"status": status})
+
+    # Return the data
+    return data
+
+
+def updateObstacle(obstacle_id: str, latitude: float, longitude: float):
+    """Updates the obstacle with the given id with the given latitude and longitude"""
+    # Get the xata client
+    client = getClient()
+
+    # Update the obstacle
+    data = client.records().update(
+        "obstacles", obstacle_id, {"latitude": latitude, "longitude": longitude}
+    )
+
+    # Return the data
+    return data
+
+
+def deleteObstacle(obstacle_id: str):
+    """Deletes the obstacle with the given id"""
+    # Get the xata client
+    client = getClient()
+
+    # Delete the obstacle
+    data = client.records().delete("obstacles", obstacle_id)
+
+    # Return the data
+    return data
+
+
+def getObstacles():
+    """Returns a list of obstacles with the following columns:
+    id, latitude, longitude"""
+
+    # Get the xata client
+    client = getClient()
+
+    # Quert Database for obstacles
+    data = client.data().query(
+        "obstacles", {"columns": ["id", "latitude", "longitude"]}
+    )
+    return data
+
+
+def getObstacle(obstacle_id: str):
+    """Returns the obstacle with the given id"""
+    # Get the xata client
+    client = getClient()
+
+    # Quert Database for obstacle
+    data = client.data().query(
+        "obstacles",
+        {"columns": ["id", "latitude", "longitude"], "filter": {"id": obstacle_id}},
+    )
+    return data
+
+def addRoombaPosition(latitude: float, longitude: float):
+    """Adds a roomba position to the database"""
+    # Get the xata client
+    client = getClient()
+
+    # Insert the roomba position
+    data = client.records().insert(
+        "roomba", {"latitude": latitude, "longitude": longitude}
+    )
+
+    # Return the data
+    return data
---
+++ Cpre288_FinalCode_Zip/Socket/main.py
@@ -0,0 +1,172 @@
+from calendar import c
+import socket
+import threading
+import numpy as np
+from msvcrt import getch
+from multiprocessing import Process
+from matplotlib import pyplot as plt
+import time
+import database as db
+
+# Define Client Variables
+HOST = "192.168.1.1"
+PORT = 288
+
+# Establish TCP connection
+conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# graph_thread = threading.Thread(target=graph_data, args=(), daemon=True)
+global exit_
+global draw_
+deg_inc = 2
+lines = int(360 / deg_inc)
+global_draw = 0
+
+
+def build_string(character, string):
+    string = string + character
+    return string
+
+
+def get_data(file_name="client_log.txt"):
+    array = np.genfromtxt(file_name, invalid_raise=False)
+    array = np.array(((array[:, 0] * np.pi) / 180, array[:, 1]))  # [theta, r]
+    r_max = np.max(np.abs(array[1:]))
+    # Convert data to polar coordinates
+    x_data = np.array((array[1, :] * np.cos(array[0, :])))
+    y_data = np.array((array[1, :] * np.sin(array[0, :])))
+
+    return x_data, y_data, r_max
+
+
+def draw_plot():
+    while True:
+        with open("client_log.txt", "r") as f:
+            num_lines = len(f.readlines())
+            if num_lines < 3:
+                continue
+        x_data, y_data, r_max = get_data()
+        # Draw Semi Circle of Object Range Seen
+        arranged_degrees = np.arange(0, 2 * np.pi, 0.001)
+        semi_circle_x = np.array(np.cos(arranged_degrees) * r_max)
+        semi_circle_y = np.array(np.sin(arranged_degrees) * r_max)
+
+        # Create Fields at Theta Degrees Interval (i)
+        i = 8
+        i_rad = i * np.pi / 180
+        arranged_field_degrees = np.arange(0, 2 * np.pi, i_rad)
+        r = np.arange(0, r_max, 0.01)
+        for j in range(int(360 / i)):
+            field_lines_x = np.array(np.cos(arranged_field_degrees[j]) * r[:])
+            field_lines_y = np.array(np.sin(arranged_field_degrees[j]) * r[:])
+            plt.plot(field_lines_x, field_lines_y, 0.1, c="black", alpha=0.3)
+        plt.plot(semi_circle_x, semi_circle_y, 0.2, c="black", alpha=0.3)
+        plt.plot(x_data, y_data, linewidth=3)
+        plt.fill_between(x_data, y_data)
+        plt.draw()
+        plt.pause(0.1)
+        plt.clf()
+
+
+def orderHandler():
+    # Infinite loop to check for orders
+    while True:
+        checkForOrder()
+        time.sleep(5)
+
+
+def checkForOrder():
+    """Checks the database for orders and sends them to the robot if ready"""
+    temp = db.getLatestOrder()
+    if temp is not None:
+        [orderLetter, status, id] = temp
+        if orderLetter is not None and status == "Preparing":
+            # send the order to the robot
+            conn.send(orderLetter.encode())
+            # print the type of orders
+            print("Orders Sent " + str(orderLetter))
+            # update the status of the order
+            setOrderStatus(id, "Delivering")
+            time.sleep(5)
+    else:
+        print("No Orders")
+        time.sleep(5)
+
+
+
+def sendBotPosition(lat: float, long: float):
+    """Sends the bot's position to the database"""
+    db.addRoombaPosition(lat, long)
+
+
+def setOrderStatus(order_id: str, status: str):
+    """Sets the status of the order in the database"""
+    db.updateOrderStatus(order_id, status)
+
+
+def messageHandler():
+    while True:
+        try:
+            message = conn.recv(1024)
+            if message:
+                string = message.decode()
+                print(string, end="", flush=True)
+                with open("client_log.txt", "a") as f:
+                    # print(f"Written {string}")
+                    f.write(string)
+        except Exception as E:
+            print(f"Error getting message: {E}")
+            globals()["exit_"] = 1
+            pass
+
+
+def keyPress():
+    while True:
+        char = getch()
+        # CyBot Characters: w, a, s, d, q, o, p
+        if char == "x".encode():
+            conn.send(char)
+            print("'x': Closing Client...")
+            globals()["exit_"] = 1
+        elif char == "q".encode() or char == "e".encode() or "l".encode:
+            f = open("client_log.txt", "w")
+            f.close()
+            conn.send(char)
+        else:
+            conn.send(char)
+
+
+def main():
+    try:
+        print("Attempting Connection...")
+        conn.connect((HOST, PORT))
+        print("Connection Successful!\n")
+    except Exception as E:
+        print(f"Error: {E}\nFailed to connect, exiting program.")
+        exit()
+    globals()["exit_"] = 0
+    print(
+        "|---Client Commands ---\n| Exit program 'x'\n| Graph Data 'g'\n| Clear data file 'c'\n| Send long string 'l'"
+    )
+
+    # Both message_thread and key_press_thread are blocking, create daemon threads (Don't need to finish for exit)
+    message_thread = threading.Thread(
+        target=messageHandler, args=(), daemon=True)
+    order_thread = threading.Thread(target=orderHandler, args=(), daemon=True)
+    key_press_thread = threading.Thread(target=keyPress, args=(), daemon=True)
+    # graph_process = Process(target=draw_plot, args=(), daemon=True)
+    message_thread.start()
+    key_press_thread.start()
+    # graph_process.start()
+    order_thread.start()
+
+    # Hold this loop until global exit is set
+    while True:
+        if exit_:
+            break
+        else:
+            pass
+
+
+if __name__ == "__main__":
+    main()
